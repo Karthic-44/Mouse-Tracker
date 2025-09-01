c3b57c013e5508d72992fa76459c468406dd0208
Karthic <karthics004@gmail.com>
2025-09-01 20:49:48 +0530

    Final application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [mousePosition, setMousePosition] = useState({x:0, y:0});
+  useEffect(() => {
+    const handelMouseMove = (event) =>{
+      setMousePosition({x: event.clientX, y: event.clientY});
+    }
+    window.addEventListener('mousemove',handelMouseMove);
+  },[]);
   return (
    <div>
+    <h2>Mouse Position</h2>
+    <p>X: {mousePosition.x}, Y: {mousePosition.y}</p>
 
    </div>
   );
