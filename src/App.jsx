d6479e760d069c544ac5239bd58eb2d703de1080
Karthic <karthics004@gmail.com>
2025-09-02 18:42:58 +0530

    Added MouseTracker

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,11 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [mousePosition, setMousePosition] = useState({x:0, y:0});
-  useEffect(() => {
-    const handelMouseMove = (event) =>{
-      setMousePosition({x: event.clientX, y: event.clientY});
-    }
-    window.addEventListener('mousemove',handelMouseMove);
-  },[]);
+
+
+  
   return (
    <div>
-    <h2>Mouse Position</h2>
-    <p>X: {mousePosition.x}, Y: {mousePosition.y}</p>
 
    </div>
   );
---
+++ src/MouseTracker.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function MouseTracker() {
+  const [mousePosition, setMousePosition] = useState({x:0, y:0});
+  useEffect(() => {
+    const handelMouseMove = (event) =>{
+      setMousePosition({x: event.clientX, y: event.clientY});
+    }
+    window.addEventListener('mousemove',handelMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handelMouseMove)
+      
+    };
+
+  },[]);
+  return (
+   <div>
+    <h2>Mouse Position</h2>
+    <p>X: {mousePosition.x}, Y: {mousePosition.y}</p>
+
+   </div>
+  );
+}
+
+export default MouseTracker
